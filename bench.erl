cf1a6d860dae802df3db9a5f055cb896687a2995
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-03-22 10:14:18 +0100

    Prefer to use erlang for the benchmark

--- bench.erl
+++ bench.erl
@@ -14,36 +14,73 @@
 %% API function
 %%
 start() ->
-    erlang:display("Starting benchmark"),
-    GridSize = argument(gridsize, 300),
-    erlang:display("Grid size: " ++ integer_to_list(GridSize)),
-    Actors = argument(actors, 1),
-    erlang:display("Number of actors: " ++ integer_to_list(Actors)),
-    Clients = argument(clients, 4),
-    erlang:display("Number of clients: " ++ integer_to_list(Clients)),
-
-    benchmark(single_actor, [GridSize, Clients]),
-    benchmark(fill_with_clients, [GridSize, Actors, Clients]),
-
-    erlang:display("Done").
+    benchmarks({300, 50, 350}, %% grid size, not really important
+               {4, 2, 6}, %% number of clients, not important neither
+               {1, 1, 8}). %% number of actors, important
 
 fill_with_clients(GridSize, Actors, Clients) ->
     Pid = reservation_multiple_actors:initialize(GridSize, Actors),
     spawn_clients(Pid, Clients, GridSize*GridSize),
-    wait_clients(Clients).
+    wait_clients(Pid, Clients).
 
 single_actor(GridSize, Clients) ->
     Pid = reservation_single_actor:initialize(GridSize),
     spawn_clients(Pid, Clients, GridSize*GridSize),
-    wait_clients(Clients).
+    wait_clients(Pid, Clients).
 %%
 %% Local functions
 %%
-argument(Name, Default) ->
-    case init:get_argument(Name) of
-        {ok, [[Val]]} -> list_to_integer(Val);
-        error -> Default
-    end.
+benchmarks({_GridSizeStart, _GridSizeStep, GridSizeStop},
+           {_ClientsStart, _ClientsStep, ClientsStop},
+           {_ActorsStart, _ActorsStep, ActorsStop},
+           GridSizeStop, ClientsStop, ActorsStop) ->
+    done;
+benchmarks({GridSizeStart, GridSizeStep, GridSizeStop},
+           {ClientsStart, ClientsStep, ClientsStop},
+           {ActorsStart, ActorsStep, ActorsStop},
+           GridSizeStop, ClientsStop, Actors) ->
+    benchmarks({GridSizeStart, GridSizeStep, GridSizeStop},
+               {ClientsStart, ClientsStep, ClientsStop},
+               {ActorsStart, ActorsStep, ActorsStop},
+               GridSizeStart,
+               ClientsStart,
+               Actors + ActorsStep);
+benchmarks({GridSizeStart, GridSizeStep, GridSizeStop},
+           {ClientsStart, ClientsStep, ClientsStop},
+           {ActorsStart, ActorsStep, ActorsStop},
+           GridSizeStop, Clients, Actors) ->
+    benchmarks({GridSizeStart, GridSizeStep, GridSizeStop},
+               {ClientsStart, ClientsStep, ClientsStop},
+               {ActorsStart, ActorsStep, ActorsStop},
+               GridSizeStart,
+               Clients + ClientsStep,
+               Actors);
+benchmarks({GridSizeStart, GridSizeStep, GridSizeStop},
+           {ClientsStart, ClientsStep, ClientsStop},
+           {ActorsStart, ActorsStep, ActorsStop},
+           GridSize, Clients, Actors) ->
+    bench(GridSize, Clients, Actors),
+    benchmarks({GridSizeStart, GridSizeStep, GridSizeStop},
+               {ClientsStart, ClientsStep, ClientsStop},
+               {ActorsStart, ActorsStep, ActorsStop},
+               GridSize + GridSizeStep,
+               Clients,
+               Actors).
+
+benchmarks({GridSizeStart, GridSizeStep, GridSizeStop},
+           {ClientsStart, ClientsStep, ClientsStop},
+           {ActorsStart, ActorsStep, ActorsStop}) ->
+    benchmarks({GridSizeStart, GridSizeStep, GridSizeStop},
+               {ClientsStart, ClientsStep, ClientsStop},
+               {ActorsStart, ActorsStep, ActorsStop},
+               GridSizeStart, ClientsStart, ActorsStart).
+
+bench(GridSize, Clients, Actors) ->
+    erlang:display("Grid size: " ++ integer_to_list(GridSize)),
+    erlang:display("Number of actors: " ++ integer_to_list(Actors)),
+    erlang:display("Number of clients: " ++ integer_to_list(Clients)),
+    benchmark(single_actor, [GridSize, Clients]),
+    benchmark(fill_with_clients, [GridSize, Actors, Clients]).
 
 spawn_clients(Pid, N, CellsToAllocate) ->
     spawn_clients(Pid, N, CellsToAllocate, N).
@@ -55,14 +92,15 @@ spawn_clients(Pid, N, CellsToAllocate, Clients) ->
     client:start(round(CellsToAllocate/Clients), Pid, self(), {5, 100, 100}),
     spawn_clients(Pid, N-1, CellsToAllocate, Clients).
 
-wait_clients(0) ->
-    done;
-wait_clients(Clients) ->
+wait_clients(Pid, 0) ->
+    exit(Pid, succes);
+wait_clients(Pid, Clients) ->
     receive
         {_, done} ->
-            wait_clients(Clients-1)
+            wait_clients(Pid, Clients-1)
     end.
 
 benchmark(Name, Args) ->
+    erlang:display({starting, Name, Args}),
     {Time, _} = timer:tc(bench, Name, Args),
-    erlang:display({time, Name, Time}).
+    erlang:display({time, Name, Args, Time}).
--- benchmark.sh
+++ benchmark.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 MODULE=bench
-if [ -z "$CLIENTS" ]; then
-  CLIENTS=4
-fi
-if [ -z "$ACTORS" ]; then
-  ACTORS=4
-fi
-if [ -z "$GRIDSIZE" ]; then
-  GRIDSIZE=300
-fi
-
-echo $CLIENTS
-erl -noshell -s $MODULE -s init stop -gridsize $GRIDSIZE -actors $ACTORS -clients $CLIENTS
+ITERATIONS=100
+erl -noshell -s $MODULE -s init stop | tee -a bench_results.txt
