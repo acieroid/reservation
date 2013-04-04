7d2f8d5a3857f9c2cb70fd223502d9c90afb1f31
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-04-04 09:40:43 +0200

    Improve benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 MODULE=bench
+BENCHMARKS=fill_with_clients
 ITERATIONS=100
-erl -noshell -s $MODULE -s init stop | tee -a bench_results.txt
+
+ACTORS_START=1
+ACTORS_STEP=1
+ACTORS_STOP=8
+
+for i in $(seq $ITERATIONS); do
+    echo "Iteration $i"
+    for actors in $(seq $ACTORS_START $ACTORS_STEP $ACTORS_STOP); do
+        for benchmark in $BENCHMARKS; do
+            echo "Starting $benchmark with $actors actors"
+            time=$(erl -noshell -s $MODULE -s init stop \
+                -benchmark $benchmark \
+                -actors $actors)
+            echo "$benchmark $actors $time" | tee -a benchmark_results.txt
+        done
+    done
+done
