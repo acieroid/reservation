f44d210b9c845c9a3b0b1d8662b159e8b4cd26d3
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-04-16 14:19:49 +0200

    Improve benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 MODULE=bench
 BENCHMARK=fill_with_clients
-ITERATIONS=100
+ITERATIONS=50
 
 GRIDSIZES=$(seq 200 50 200)
-CLIENTS="1 5 10 100 1000"
+CLIENTS="5"
 GRIDCOMPLETIONS=$(seq 25 25 100)
+SPECIFIC=$(seq 25 25 100)
 ACTORS=$(seq 0 1 7)
 
 mkdir -p benchmark_results
@@ -15,20 +16,23 @@ for i in $(seq $ITERATIONS); do
     for actors in $ACTORS; do
         for clients in $CLIENTS; do
             for completion in $GRIDCOMPLETIONS; do
-                for gridsize in $GRIDSIZES; do
-                    echo "Starting $benchmark with actors:$actors, clients:$clients, completion:$completion, gridsize:$gridsize, iteration:$i"
-                    time=$(erl -noshell -s $MODULE -s init stop \
-                        -benchmark $BENCHMARK \
-                        -actors $actors \
-                        -gridsize $gridsize \
-                        -grid_completion $completion \
-                        -clients $clients)
-                    echo "$BENCHMARK $actors $time" | tee -a \
-                        benchmark_results/grid${gridsize}_clients${clients}_completion${completion}.txt
+                for specific in $SPECIFIC; do
+                    for gridsize in $GRIDSIZES; do
+                        echo "Starting $benchmark with actors:$actors, clients:$clients, completion:$completion, specific:$specific, gridsize:$gridsize, iteration:$i"
+                        time=$(erl -noshell -s $MODULE -s init stop \
+                            -benchmark $BENCHMARK \
+                            -actors $actors \
+                            -gridsize $gridsize \
+                            -grid_completion $completion \
+                            -percent_specific $specific \
+                            -clients $clients)
+                        echo "$BENCHMARK $actors $time" | tee -a \
+                            benchmark_results/grid${gridsize}_clients${clients}_completion${completion}.txt
+                        echo
+                    done
                 done
             done
         done
     done
 done
-
 
